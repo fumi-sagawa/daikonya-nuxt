b7666b15cd7f073cbd60f7cce6352c9511f3b9fc
fumi-sagawa <sagawa.fumi@gmail.com>
2024-02-29 21:43:14 +0900

    店休日のアップデート

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@ main
     figure.main: img(src="../assets/images/top_main_mobile.jpg", alt="")
     figure.grad: img(src="../assets/images/top_main_mobile_grad.png", alt="")
   .about
-    .offday: div.inner <span class="title">今月の店休日</span><br>2/4(日), 2/11(日), 2/12(月), 2/18(日), 2/25(日)
+    .offday: div.inner <span class="title">今月の店休日</span><br>3/3(日), 3/4(月), 3/10(日), 3/17(日), 3/20(祝), 3/24(日), 3/31(日)
     figure.noren: img(src="../assets/images/noren.jpg", alt="")
     figure.tennai: img(src="../assets/images/tennai.jpg", alt="")
     h2#about_anime 台所から、<br>腕によりをかけて
