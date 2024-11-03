3c9d52e4124919120fe563165f45cda96f04c2c5
fumi-sagawa <sagawa.fumi@gmail.com>
2024-11-03 10:33:45 +0900

    店休日の変更

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@ main
     figure.main: img(src="../assets/images/top_main_mobile.jpg", alt="")
     figure.grad: img(src="../assets/images/top_main_mobile_grad.png", alt="")
   .about
-    .offday: div.inner <span class="title">今月の店休日</span><br>9/1(日), 9/8(日), 9/15(日), 9/16(月), 9/22(日), 9/23(月), 9/29(日)
+    .offday: div.inner <span class="title">今月の店休日</span><br>11/3(日), 11/4(月), 11/10(日), 11/17(日), 11/24(日)
     figure.noren: img(src="../assets/images/noren.jpg", alt="")
     figure.tennai: img(src="../assets/images/tennai.jpg", alt="")
     h2#about_anime 台所から、<br>腕によりをかけて
