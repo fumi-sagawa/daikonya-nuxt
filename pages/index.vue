7b3f1e14b0f645e573bf003f4074223210d0e220
fumi-sagawa <sagawa.fumi@gmail.com>
2026-04-14 17:14:04 +0900

    日付の更新

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@ main
     figure.main: img(src="../assets/images/top_main_mobile.jpg", alt="")
     figure.grad: img(src="../assets/images/top_main_mobile_grad.png", alt="")
   .about
-    .offday: div.inner <span class="title">今月の店休日</span><br>4/5(日), 4/6(月), 4/12(日), 4/19(日), 4/26(日)
+    .offday: div.inner <span class="title">今月の店休日</span><br>4/5(日), 4/6(月), 4/12(日), 4/19(日), 4/26(日), 4/29(祝日)
 
     figure.noren: img(src="../assets/images/noren.jpg", alt="")
     figure.tennai: img(src="../assets/images/tennai.jpg", alt="")
